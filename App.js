9075cad089b60316f0afcdf08d60216a13cf026b
EmRenteria <emma.renteria5@gmail.com>
2021-02-27 11:39:42 -0600

    Added Home page

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 export default function App() {
+  var [pressed, setPressed] = useState(false);
+  var press = () => {
+    setPressed(true);
+    console.log("Press button")
+  }
+  var page = (
+    <Pressable onPress={press}>
+      <Home />
+    </Pressable>
+  );
+  if(pressed) {
+    page = (
+      <Login />
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Welcome to the QuickPick App!</Text>
-      <StatusBar style="bold" />  
+      {page}
     </View>
   );
 }
