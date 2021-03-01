93afea26fde60c7f3fff32f22a74cd480c6e295b
halelifra <hallie.franco@ttu.edu>
2021-03-01 02:13:06 -0600

    Took Out Create Account part

--- App.js
+++ App.js
@@ -21,16 +21,7 @@ export default function App() {
       <Login />
     )
   }
-  var page = (
-    <Pressable onPress={press}>
-      <Login />
-    </Pressable>
-  );
-  if(pressed) {
-    page = (
-      <Create_account />
-    )
-  }
+  
 
 return (
   <View style={styles.container}>
