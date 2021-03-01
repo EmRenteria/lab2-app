169d235a34b6ca1916b22983d721615e835c5dee
halelifra <hallie.franco@ttu.edu>
2021-02-28 23:22:44 -0600

    Back to how it was - did not work

--- App.js
+++ App.js
@@ -22,43 +22,7 @@ export default function App() {
     )
   }
 
-  onLogin() ;
-    const { username, password } = this.state;
-
-    Alert.alert('Credentials', `${username} + ${password}`);
-  }
-
-  render() 
-    return (
-      <View style={styles.container}>
-        <TextInput
-          value={this.state.username}
-          onChangeText={(username) => this.setState({ username })}
-          placeholder={'Username'}
-          style={styles.input}
-        />
-        <TextInput
-          value={this.state.password}
-          onChangeText={(password) => this.setState({ password })}
-          placeholder={'Password'}
-          secureTextEntry={true}
-          style={styles.input}
-        />
-        
-        <Button
-          title={'Login'}
-          style={styles.input}
-          onPress={this.onLogin.bind(this)}
-        />
-      </View>
-    );
-  
-
-  return (
-    <View style={styles.container}>
-      {page}
-    </View>
-  );
+ 
 
 const styles = StyleSheet.create({
   container: {
