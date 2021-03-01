7399310d2db92ea6bae1f5d52a508fbd0ac38dd8
halelifra <hallie.franco@ttu.edu>
2021-03-01 00:52:53 -0600

    Login Set Up

--- pages/Login.js
+++ pages/Login.js
@@ -1,13 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Image,  } from 'react-native';
 
 export default function Login() {
-  return (
-    <View style={styles.container}>
-      <Text>Customer Login in</Text>
-    </View>
-  );
+  render();
+    state={
+      email:"",
+      password:""
+    }
+    return (
+      <view style={styles.logo}>
+        <Text>QuickPick Log in </Text>
+      </view>
+    )
+    return (
+      <View style={styles.inputView} >
+          <TextInput  
+            style={styles.inputText}
+            placeholder="Email..." 
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({email:text})}/>
+        </View>
+    )
+    return (
+      <TouchableOpacity>
+          <Text style={styles.forgot}>Forgot Password?</Text>
+        </TouchableOpacity>
+    )
+    return ( 
+      <TouchableOpacity>
+          <Text style={styles.loginText}>Signup</Text>
+        </TouchableOpacity>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +40,38 @@ const styles = StyleSheet.create({
     backgroundColor: '#9DC183',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo:{
+    fontWeight:"bold",
+    fontSize:50,
+    color:"#fb5b5a",
+    marginBottom:40
+  },
+  inputView:{
+    width:"80%",
+    backgroundColor:"#465881",
+    borderRadius:25,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20
+  },
+  inputText:{
+    height:50,
+    color:"white"
+  },
+  forgot:{
+    color:"white",
+    fontSize:11
+  },
+  loginBtn:{
+    width:"80%",
+    backgroundColor:"#fb5b5a",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
   },
 });
