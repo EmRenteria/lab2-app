995b93357e0159c4f47fbf027df27adce0583586
halelifra <hallie.franco@ttu.edu>
2021-03-01 21:40:39 -0600

    Changed useraccount

--- pages/UserAccount.js
+++ pages/UserAccount.js
@@ -1,41 +1,112 @@
-import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 
 export default function Payment() {
   
 
  
 
-    return (
-      <View style={styles.container}>
-  
-        
-        <StatusBar style="auto" />
+  return (
+    <View style={styles.container}>
 
-        <View Style={styles.baseText}>
-            <Text>Hello</Text>
-            <View Style={styles.innerText}>
-                <Text>Hallie!</Text>
-            </View>
-        </View>
-  
-  
-        
+      
+      <StatusBar style="auto" />
+
+
+      <View Style={styles.text}>
+        <Text>Hello Hallie! Here is your Account information.</Text>
       </View>
-    );
-  }
+
+      <View Style={styles.text}>
+        <Text>Your Email:</Text>
+      </View>
+
+      <View Style={styles.logo}>
+        <Text>Hallie.franco@ttu.edu</Text>
+      </View>
+
+      <View Style={styles.text}>
+        <Text>Your Phone Number:</Text>
+      </View>
+
+      <View Style={styles.logo}>
+        <Text>(123)456-7890</Text>
+      </View>
+
+      <View Style={styles.text}>
+        <Text>Your Town:</Text>
+      </View>
+
+      <View Style={styles.logo}>
+        <Text>Lubbock</Text>
+      </View>
+
+      <View Style={styles.text}>
+        <Text>Preferred Store:</Text>
+      </View>
+
+      <View Style={styles.logo}>
+        <Text>Target</Text>
+      </View>
+
+      <View Style={styles.text}>
+        <Text>Preferred Payment Method</Text>
+      </View>
+
+      <View Style={styles.logo}>
+        <Text>Apple Pay</Text>
+      </View>
+
+      <TouchableOpacity>
+        <Text style={styles.edit_button}>Edit Account Information</Text>
+      </TouchableOpacity>
+
+
+      
+     
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#9DC183",
-        alignItems: "center",
-        justifyContent: "center",
-      },
-  baseText: {
-    fontWeight: 'bold'
+  container: {
+    flex: 1,
+    backgroundColor: "#9DC183",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  innerText: {
-    color: 'red'
-  }
+
+  image: {
+    marginBottom: 200,
+  },
+
+  
+ 
+  logo:{
+    fontWeight:"bold",
+    fontSize:50,
+    color:"red",
+    marginBottom:40
+  },
+
+  text:{
+    fontWeight:"bold",
+    fontSize:50,
+    color:"red",
+    marginBottom:40
+  },
+  edit_button: {
+    height: 30,
+    marginBottom: 30,
+  },
 });
